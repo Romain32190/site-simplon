2a62781f3967cab9133213fba22fc229d46b5044
mouad13 <mouad.rifaielmoubarik@gmail.com>
2017-08-02 11:47:30 +0200

    migratin Blog

---
+++ database/migrations/2017_08_02_092930_blog.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Blog extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blog', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('titre');
+            $table->string('article');
+            $table->integer('auteur');
+            $table->integer('note');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('blog');
+    }
+}
