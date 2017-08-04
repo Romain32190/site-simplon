3d8c59787a546980a3ed8da994bf269aaf491c50
Morel <MorelCisse19@gmail.com>
2017-08-04 15:42:12 +0000

    Add ShakeOpacity

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title> Acceuil </title>
+    <link rel="stylesheet" type="text/css" href="/css/csshake.min.css">
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.css">
@@ -26,7 +27,7 @@
 
             <div class="content">
 
-                <div class="animated bounceInDown">
+            <div class="animated bounceInDown shake-opacity">
                     <span class="simplon_auch"> SIMPLON AUCH </span>
                 </div>
 
