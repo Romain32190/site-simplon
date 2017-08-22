ac1358ae87f1f1ed5fb01576eb255badeb7b0c19
r-sede <r-sede@orange.fr>
2017-08-22 11:23:59 +0200

    change methode recup articale

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,14 +1,3 @@
-<?php 
-try {
-$bdd= new PDO('mysql:host=localhost;dbname='.env('DB_DATABASE').';charset=utf8', env('DB_USERNAME'), env('DB_PASSWORD'));
-
-  }
-  catch (Exception $e) {
-    die('Erreur : ' .$e->getMessage());
-  }
-// Récupère les 10 derniers articles écrits
-$blog= $bdd->query('SELECT titre, userName, article, blog.created_at FROM blog JOIN users ON blog.auteur=users.id ORDER BY blog.created_at DESC ');
- ?>
 <!DOCTYPE html>
 <html>
   <head>
@@ -21,22 +10,10 @@ $blog= $bdd->query('SELECT titre, userName, article, blog.created_at FROM blog J
   <body class="image">
 
     <h1>Blog Simplon Auch</h1>
-<!-- <a type="application/rss+xml"
+<a type="application/rss+xml"
    href="http://127.0.0.1:8000/flux">Flux RSS de cette page
-</a> -->
+</a>
 
- <?php while($art = $blog->fetch()){ ?>                
-        
-        <div>
-          <div>
-            <h3><?= $art['titre'] ?></h3>
-          </div>
 
-            <div><?=$art['article']?><div>
-            <div><?=$art['userName']?></div>
-            <div><?=$art['created_at'] ?></div>
-        </div>   
-        
-        <?php } ?>
   </body>
 </html>
